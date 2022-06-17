e26d9973113d57cc7d27d92f7d6119e0c2460455
yannick Djemani <yanger842@gmail.com>
2022-06-17 02:29:54 -0400

    Ajout de la couche fournisseur service

--- src/main/java/com/afridevteam/gestionstock/service/FournisseurService.java
+++ src/main/java/com/afridevteam/gestionstock/service/FournisseurService.java
@@ -1,19 +1,6 @@
 package com.afridevteam.gestionstock.service;
 
-import com.afridevteam.gestionstock.dto.ClientDto;
 import com.afridevteam.gestionstock.dto.FournisseurDto;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface FournisseurService {
-    Optional<FournisseurDto> save(ClientDto dto);
-
-    Optional<FournisseurDto> findById(Long id);
-
-    Optional<FournisseurDto> findByNom(String name);
-
-    List<FournisseurDto> findAll();
-
-    //void delete(Long id);
+public interface FournisseurService extends EntityService<FournisseurDto> {
 }
