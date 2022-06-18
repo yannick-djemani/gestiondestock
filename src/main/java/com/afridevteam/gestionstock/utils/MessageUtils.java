d66294bc76ecbd5427fca00cc6208b349b5abc5a
yannick Djemani <yanger842@gmail.com>
2022-06-18 01:56:20 -0400

    Implementation du de ArticleService, CommandeFournisseurService, Entreprise service et cration de toutes les interfaces

---
+++ src/main/java/com/afridevteam/gestionstock/utils/MessageUtils.java
@@ -0,0 +1,8 @@
+package com.afridevteam.gestionstock.utils;
+
+public class StringUtils {
+    
+    public static final String MESSAGE_F = "Aucune message {} avec le {}={} n'a ete trouve dans la BD";
+    public static final String MESSAGE_M = "Aucun message {} avec le {}={} n'a ete trouve dans la BD";
+
+}
